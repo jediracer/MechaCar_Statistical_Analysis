7f4df6e636a489b73f7eaef4a34f9dbc4b69a80c
Jared Behler <jediracer13@gmail.com>
2021-09-18 17:36:21 -0400

    Update MechaCarChallenge.R, README and add images

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,10 @@
+#Deliverable 1
 library(dplyr)
 mechaCarMPG <- read.csv('MechaCar_mpg.csv')
 lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg, data=mechaCarMPG)
 summary(lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg, data=mechaCarMPG))
+
+#Deliverable 2
+suspensionCoil <- read.csv('Suspension_Coil.csv')
+total_summary <- suspensionCoil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+lot_summary <- suspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # MechaCar_Statistical_Analysis
+
+## Linear Regression to Predict MPG
+- The variables/coefficients that provided a non-random amount of variance are: vehicle_length(intercept), ground_clearance and mpg.
+- The slope of the linear model is not considered to be zero as the p-value(6.712e-11) is less than the significance level od 0.05%
+- This liner model does predict the mpg of the MechaCar prototypes effectively as the R-squared value(0.7119) is greater than .07. 
+- Results:
+
+  ![Results](https://github.com/jediracer/MechaCar_Statistical_Analysis/blob/main/images/MechaCar_mpg_prediction.png)	
